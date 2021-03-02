4cd21eaa49aa134e2d8870535750e3a6b35e5fa3
Jonatan Rivilla Alamo <jonatan@jacoti.com>
2021-03-02 21:29:53 +0100

    removed www. from domain name

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -12,7 +12,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(${NGINX_HOST} www.${NGINX_HOST})
+domains=(${NGINX_HOST})
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
